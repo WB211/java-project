1beb5fbcdc0d5157cadcf5a2761f98eae670acc1
Bruno <weblogs21@gmail.com>
2020-03-25 21:38:25 -0500

    primer commit de mi web

---
+++ home.java
@@ -0,0 +1,22 @@
+
+public class weyya {
+
+	public static void public static void main(String[] args) {
+
+		System.out.print("Hola mundo! :v");
+		
+	}
+
+}
+
+public class variables {
+
+	public static void public static void main(String[] args) {
+		
+		byte edad;
+
+		edad= 35;
+
+		System.out.print("edad");
+	}
+}
